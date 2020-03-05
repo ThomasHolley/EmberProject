a10f3adc4591c25c5c43dccb2fbfc77d92fad48d
ThomasHolley <55082849+ThomasHolley@users.noreply.github.com>
2020-03-05 11:58:28 +0100

    Add fomentic + reset button

--- app/routes/ex1.js
+++ app/routes/ex1.js
@@ -1,8 +1,13 @@
-import Route from '@ember/routing/route';
-import Note from 'ember-project/utils/note-class';
+import Route from "@ember/routing/route";
+import Note from "ember-project/utils/note-class";
 
 export default Route.extend({
-  model(){
+  model() {
     return Note.create();
+  },
+  actions: {
+    reset(m) {
+      m.set("content");
+    }
   }
 });
--- app/utils/note-class.js
+++ app/utils/note-class.js
@@ -4,6 +4,8 @@ import EmberObject, { computed } from '@ember/object';
 export default EmberObject.extend({
   MAX:100,
   content:'Entrez votre texte',
+  style: 'info',
+  info: '',
   size: computed('content', function(){
     return this.get('MAX')-this.get('content').length;
   })
